4e8e2fe764972d50d2a8de2d4a6e8c0f995a6b39
igsmx <73009311+igsmx@users.noreply.github.com>
2020-10-17 00:34:34 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/READMEIGS':
-    ensure  => file,
-    content => 'This is a content 00:25',
-    owner   => 'tss',
-  }
-  file {'/root/READMEIGS':
-  owner => 'root',
-  }
 }
